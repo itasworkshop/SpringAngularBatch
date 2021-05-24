6586c54aded4eab383bb82caf8c1b073a4415241
Venkteshwar Tripathi <tripathivenkteshwar@gamil.com>
2021-05-24 18:52:49 +0530

    added webtest files

---
+++ students/Venkteshwar/testweb/src/main/java/com/mysite/web/StudentController.java
@@ -0,0 +1,29 @@
+package com.mysite.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mysite.service.StudentService;
+
+@Controller
+public class StudentController {
+	
+	private StudentService studentService;
+
+	
+	@Autowired
+	public void setStudentService(StudentService studentService) {
+		this.studentService = studentService;
+	}
+	
+	@RequestMapping(value = "/" ,method= RequestMethod.GET)
+	public String showAll(Model model) {
+		model.addAttribute("students",studentService.findAll() );
+		//System.out.println(studentService.findAll());
+		return "student/list";
+	}
+
+}
---
+++ students/Venkteshwar/testweb/src/main/webapp/WEB-INF/views/jsp/student/list.jsp
@@ -0,0 +1,27 @@
+<%@ page session="false"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Student App</title>
+</head>
+<body>
+	<table class="table table-striped">
+		<thead>
+			<tr>
+				<th>Student Name</th>
+				<th>Student RollNo</th>
+			</tr>
+		</thead>
+		<c:forEach var="student" items="${students}">
+			<tr>
+				<td>${student.name}</td>
+				<td>${student.rollNo}</td>
+			</tr>
+		</c:forEach>
+	</table>
+</body>
+</html>
